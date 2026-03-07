c6ab95923c9a2566811aaf5c7a829dd5442e7edd
xuanduong <estella113pro@gmail.com>
2026-03-07 16:13:41 +0700

    Update: 0.011

--- frontend/pages/MusicPlayer/MusicPlayer.jsx
+++ frontend/pages/MusicPlayer/MusicPlayer.jsx
@@ -7,6 +7,13 @@ const trackList = [
   { id: 3, title: "Nàng Thơ", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772818121/N%C3%A0ng_Th%C6%A1_Ho%C3%A0ng_D%C5%A9ng_Official_MV_lg0eke.mp3" },
   { id: 4, title: "漫步香港1999", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772817709/%E5%B8%83%E9%B2%81%E6%98%94_-_%E6%BC%AB%E6%AD%A5%E9%A6%99%E6%B8%AF1999_zilulo.mp3" },
   { id: 5, title: "Ordinary", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772817716/Alex_Warren_-_Ordinary_Lyrics_rmh4wv.mp3" },
+  { id: 6, title: "Đơn_Giản_Anh_Yêu_Em", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772874413/%C4%90%C6%A1n_Gi%E1%BA%A3n_Anh_Y%C3%AAu_Em_-_H%E1%BB%93_Qu%E1%BB%91c_Vi%E1%BB%87t_Lyric_Video_Nh%E1%BA%A1c_Hot_Tiktok_uaksmr.mp3" },
+  { id: 7, title: "CÓ_CHẮC_YÊU_LÀ_ĐÂY", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772874404/C%C3%93_CH%E1%BA%AEC_Y%C3%8AU_L%C3%80_%C4%90%C3%82Y___S%C6%A0N_T%C3%99NG_M-TP___Lyric_Video_rg5cPEf18IU_idp1ds.mp3" },
+  { id: 8, title: "Lan_Man", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772874404/Lan_Man_Ronboogz_Lyrics_Video_i06j4p.mp3" },
+  { id: 9, title: "Dành_Cho_Em", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772874404/Ho%C3%A0ng_D%C5%A9ng_x_Orange_-_D%C3%A0nh_Cho_Em_-_Live_at_y%C3%AAn_Concert_ue5kbk.mp3" },
+  { id: 10, title: "Phép_Màu", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772874404/Ph%C3%A9p_M%C3%A0u_%C4%90%C3%A0n_C%C3%A1_G%E1%BB%97_OST_-_Mounter_x_MAYDAYs_Minh_T%E1%BB%91c_Official_MV_gkemhh.mp3" },
+  { id: 11, title: "1000 Ánh Mắt", url: "https://res.cloudinary.com/dudh0rcav/video/upload/v1772874404/AJDEu1-nSTI_proym8.mp3" },
+  
 ];
 
 const MusicPlayer = () => {
